a9d80643f443515cdd08d6ba63f8ccb57e4348d0
Li-hub-san <ligia.baptista.santos@gmail.com>
2022-01-14 10:59:42 +0000

    Continued menu 1

--- src/Application.java
+++ src/Application.java
@@ -1,23 +1,42 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Application {
 
-  public static void main(String[] args) {
+    static Random ran = new Random();
+    static Scanner sc = new Scanner(System.in);
 
-    Scanner sc =new Scanner(System.in);
-    initialMenu();
-    MagicLamp firstMagicLamp = new MagicLamp(2);
+    public static void main(String[] args) {
 
-    Genie firstGenie = firstMagicLamp.rub(3);
-//    System.out.println(firstGenie);
+        System.out.println("Welcome to Aladdin factory!" + "\n" + "Create Magic Lamp(1) | Exit(2)");
 
-    if (firstGenie instanceof Demon) {
-      firstMagicLamp.recharge((Demon) firstGenie);
+        int firstChoice = sc.nextInt();
+//        int numberOfWishes = sc.nextInt();
+
+        initialMenu(firstChoice);
     }
-  }
 
-  public static void initialMenu(){
-    System.out.println("Welcome to Aladdin factory!" + "\n" + "Create Magic Lamp(1) | Exit(2)");
-  }
+    public static void initialMenu(int firstChoice) {
+
+        if (firstChoice == 1) {
+            MagicLamp firstMagicLamp = new MagicLamp(generateRandomGenie());
+
+            Genie firstGenie = firstMagicLamp.rub(3);
+            firstGenie.grantWish();
+
+
+            if (firstGenie instanceof Demon) {
+                firstMagicLamp.recharge((Demon) firstGenie);
+            }
+        } else {
+            System.out.println("Ohh no! Hope to see you again.");
+            System.exit(0);
+        }
+
+    }
+
+    public static int generateRandomGenie() {
+        return ran.nextInt(10) + 1;
+    }
 
 }
